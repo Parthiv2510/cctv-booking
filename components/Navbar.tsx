c04da0db0112005a7f58e893519ccc15d1926544
Parthiv2510 <parthivkm73@gmail.com>
2026-03-13 01:38:26 +0530

    UI: Complete redesign of the Header for a more premium, beautiful look

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,57 @@
+'use client';
+
 import Link from 'next/link';
+import { motion, useScroll, useSpring } from 'framer-motion';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
-    <nav className={`${styles.nav} glass`}>
-      <div className={`container ${styles.navContainer}`}>
-        <Link href="/" className={styles.logo}>
-          <span>The Pacific</span>
-        </Link>
-        <ul className={styles.navLinks}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/services">Services</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/book">Book</Link></li>
-        </ul>
-        <Link href="/book" className={styles.ctaLink}>Buy Now</Link>
-      </div>
-    </nav>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} glass`}>
+        <div className={`container ${styles.navContainer}`}>
+          <Link href="/" className={styles.logo}>
+            <span className={styles.logoPrefix}>THE</span>
+            <span className={styles.logoMain}>PACIFIC</span>
+          </Link>
+          
+          <ul className={styles.navLinks}>
+            <li>
+              <Link href="/">
+                <span>Home</span>
+                <span className={styles.linkDot}></span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/services">
+                <span>Services</span>
+                <span className={styles.linkDot}></span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <span>About</span>
+                <span className={styles.linkDot}></span>
+              </Link>
+            </li>
+          </ul>
+
+          <div className={styles.actions}>
+            <Link href="/book" className={styles.buyBtn}>
+              Deploy System
+            </Link>
+          </div>
+        </div>
+      </nav>
+      {/* Subtle Scroll Progress Bar */}
+      <motion.div className={styles.progressBar} style={{ scaleX }} />
+    </header>
   );
 };
 
